48edfe967f835d18d354588d1941ef11cc689d95
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2025-01-06 04:58:53 +0000

    feat: implement bitpay

--- backend/api/utils/settings.py
+++ backend/api/utils/settings.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent
 
 
 class Settings(BaseSettings):
+    ENV: str = config("ENVIRONMENT", "dev")
     PROJECT_NAME:str = "Monika Verein API"
     PROJECT_VERSION: str = "1.0.0"
 
@@ -17,5 +18,10 @@ class Settings(BaseSettings):
     DB_NAME : str = config("DB_NAME")
     DB_TYPE : str = config("DB_TYPE", "sqlite")
     DATABASE_URL : str = f"DBql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+    BITPAY_API_TOKEN : str = config("BITPAY_API_TOKEN")
+    NOTIFICATION_EMAIL : str = config("NOTIFICATION_EMAIL")
+    NOTIFICATION_URL : str = config("NOTIFICATION_URL")
+    MERCHANT_NAME: str = config("MERCHANT_NAME", "HNG SCRUM")
 
 settings = Settings()

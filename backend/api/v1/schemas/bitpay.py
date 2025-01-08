0d804c181f9f84385cb5de33846425816e258f57
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2025-01-08 00:16:28 +0000

    chore: clean up

--- backend/api/v1/schemas/bitpay.py
+++ backend/api/v1/schemas/bitpay.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, PositiveFloat
 from api.v1.schemas.base_schema import ResponseBase
 
 
 class CreateInvoice(BaseModel):
     """Create invoice input"""
-    amount: float
+    amount: PositiveFloat
     currency: str = "USD"
-    email: str
+    email: EmailStr
 
 
 class CreateInvoiceResponse(ResponseBase):
